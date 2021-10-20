f79539f2475668bbd58aa5c413d155a1ef67e39f
kotaro0119 <92850766+kotaro0119@users.noreply.github.com>
2021-10-20 17:26:16 +0900

    Add files via upload

---
+++ js/slideShowV3.js
@@ -0,0 +1,69 @@
+let count=1;
+const src ="https://www.takushoku-u.ac.jp/summary/images/summary_successive-chancellor_img_"
+let mainElement = document.querySelector("div.main>img");
+let thumbnailsElement = document.querySelector("div.thumbnails");
+const MIN = 1;
+const MAX = 19;
+let url;
+function left() {
+  count=count-1;
+if(count < MIN){
+  count = MAX;
+  thumbnailsElement.children[0].classList.remove("selected");
+}else{
+  thumbnailsElement.children[count].classList.remove("selected");
+}
+if(count < 10){
+  url = src + "0" + count + ".jpg";
+}else{
+  url = src + count + ".jpg";
+}
+  mainElement.setAttribute('src', url);
+  thumbnailsElement.children[count-1].classList.add("selected");
+}
+function right() {
+  count=count+1;
+
+if(count > MAX){
+  count = MIN;
+  thumbnailsElement.children[MAX-1].classList.remove("selected");
+}else{
+  thumbnailsElement.children[count-2].classList.remove("selected");
+}
+if(count < 10){
+  url = src + "0" + count + ".jpg";
+}else{
+  url = src + count + ".jpg";
+}
+  mainElement.setAttribute('src', url);
+  thumbnailsElement.children[count-1].classList.add("selected");
+}
+
+
+let timer;
+let nowPlaying = false;
+function play(){
+  if(nowPlaying == false){
+nowPlaying = true;
+autoPlay();
+}
+}
+function autoPlay(){
+
+    right();
+    timer = setTimeout(function(){
+      autoPlay()},1000);
+
+}
+function stop(){
+  clearTimeout(timer);
+  nowPlaying = false;
+}
+function reset(){
+  stop();
+  thumbnailsElement.children[count-1].classList.remove("selected");
+  url = src + "01.jpg";
+  mainElement.setAttribute('src', url);
+  thumbnailsElement.children[MIN-1].classList.add("selected");
+  count = 1;
+}
